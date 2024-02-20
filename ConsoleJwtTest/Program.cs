3b8a9f4079d5fb98b2bf773ed793cdf2250e81d6
Aiman Salamah <aiman_salamah@outlook.com>
2024-02-20 14:10:24 +0300

    Add project files.

---
+++ ConsoleJwtTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+var jwt = Console.ReadLine();
+if(!string.IsNullOrEmpty(jwt))
+{
+    var handler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+    var tokens = handler.ReadToken(jwt) as System.IdentityModel.Tokens.Jwt.JwtSecurityToken;
+    foreach (var token in tokens.Claims)
+    {
+        Console.WriteLine($"Type:{token.Type}, Value:{token.Value}");
+    }
+    var user = new ClaimsPrincipal(new ClaimsIdentity(tokens.Claims, "Auth"));
+    var username = user.FindFirst(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Sub).Value;
+}
